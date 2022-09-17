146d15b1c5f16d29d22e3846abdb33f4f4d2e204
Allan <github email address>
2022-09-17 20:08:07 +0300

    create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < Sinatra::Base
+
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # Get all users
+  get '/users' do
+    users = User.all
+    users.to_json
+  end
+
+  # Create a new user
+  post '/users' do 
+    user = User.create(
+      first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      email: params[:email], 
+      password: params[:password], 
+      password_confirmation: params[:password_confirmation])
+    user.to_json
+  end
+
+  # Delete a user
+  delete '/users/:id' do
+    user = User.all.find(params[:id])
+    user.delete
+    user.to_json
   end
 
 end
