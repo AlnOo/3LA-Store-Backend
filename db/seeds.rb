bf34983cec6365385e588beb8ed18e52a422c957
Allan <github email address>
2022-09-18 12:13:55 +0300

    added username password creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+allan = User.create(first_name: "Allan", last_name: "Odhis", email: "alnothigo@gmail.com", password: "12345678", password_confirmation: "12345678")
+
 puts "✅ Done seeding!"
