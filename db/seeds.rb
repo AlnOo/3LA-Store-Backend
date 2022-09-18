9a3a6402c1945b6ba792f57391fe4a1be3275a0d
Allan <github email address>
2022-09-18 15:31:30 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-puts "🌱 Seeding spices..."
-
-# Seed your database here
+puts "Seeding spices..."
 
 allan = User.create(first_name: "Allan", last_name: "Odhis", email: "alnothigo@gmail.com", password: "12345678", password_confirmation: "12345678")
 
-puts "✅ Done seeding!"
+puts "Done seeding!"
